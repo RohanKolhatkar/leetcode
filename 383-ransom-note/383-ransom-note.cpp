faa6db1f835793441b74684f10ed42d1fd37e921
Rohan kolhatkar <rohankolhatkar930@gmail.com>
2022-08-25 09:49:05 +0530

    Time: 23 ms (59.46%), Space: 8.6 MB (89.81%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,20 +1,34 @@
+// class Solution {
+// public:
+//     bool canConstruct(string ransomNote, string magazine) {
+// 	    // you can also use unordered_map<int, int> m; here
+// 		// since we're just dealing with lowercase English letters, 
+//         // we can just use an array of length 26 to store the frequency of them
+//         int m[26] = {0};
+//         // count each character
+//         for(char c : magazine) m[c - 'a']++;
+        
+        
+        
+//         // check if it can be found in m and substract by 1 
+//         for(char c : ransomNote) {
+//             m[c-'a']--;
+// 			// if it is less than 0, it means it cannot be constructed from magazine
+//             if(m[c - 'a'] < 0) return false;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-	    // you can also use unordered_map<int, int> m; here
-		// since we're just dealing with lowercase English letters, 
-        // we can just use an array of length 26 to store the frequency of them
+	   
         int m[26] = {0};
-        // count each character
-        for(char c : magazine) m[c - 'a']++;
-        
-        
-        
-        // check if it can be found in m and substract by 1 
-        for(char c : ransomNote) {
-            m[c-'a']--;
-			// if it is less than 0, it means it cannot be constructed from magazine
-            if(m[c - 'a'] < 0) return false;
+        for(char x : magazine) m[x - 'a']++;      
+        for(char x : ransomNote) {
+            m[x-'a']--;
+            if(m[x - 'a'] < 0) return false;
         }
         return true;
     }
