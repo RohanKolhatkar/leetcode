dbe8952c2489d6b55c16ff4326f69855dd7961f0
Rohan kolhatkar <rohankolhatkar930@gmail.com>
2022-10-15 12:14:16 +0530

    Time: 79 ms (41.51%), Space: 20.7 MB (56.74%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,5 +1,5 @@
 // the logic here is to use that the anagrams that are taken if we will sort the anagrams they will become the ssame string and if they are same after sorted that means thay are angram of each other.
-
+// do not use the basic of logic of putting in a ordered hashmap and checking all with one fixed that wount work here what we do in a checking ermutation or anagram question.
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -16,6 +16,9 @@ public:
         return ans;
     }
 };
+
+
+
 
 
 
