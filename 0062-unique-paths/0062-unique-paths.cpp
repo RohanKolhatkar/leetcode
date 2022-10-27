e730183e46087662c9138894cde8418c40686064
Rohan kolhatkar <rohankolhatkar930@gmail.com>
2022-10-27 20:20:31 +0530

    Time: 3 ms (46.27%), Space: 6.2 MB (64.02%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/unique-paths/discuss/22954/C%2B%2B-DP
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> cur(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }
+};
