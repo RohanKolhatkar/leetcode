f6364f05f9cc0e6dfa9ef432a00164e1704719a2
Rohan kolhatkar <rohankolhatkar930@gmail.com>
2022-10-27 19:26:09 +0530

    Time: 480 ms (30.25%), Space: 0B (100.00%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -1,7 +1,14 @@
 # Write your MySQL query statement below
 
+#  it is always a best and good practice to have the count of ony
+# the distinct one it saves computation time.
+# select class
+# from courses
+# group by class
+# having count(student) >=5;
+
 
 select class
 from courses
 group by class
-having count(student) >=5;
+having count(distinct student) >=5;
