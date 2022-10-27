da345d498b462c91a7822c7f23109e3def8b3b46
Rohan kolhatkar <rohankolhatkar930@gmail.com>
2022-10-27 15:23:02 +0530

    Time: 364 ms (95.90%), Space: 0B (100.00%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -9,5 +9,7 @@
 # select p.FirstName, p.LastName, a.City, a.State
 # from Person as p left join Address as a on p.PersonId = a.PersonId;
 
+
+# // this is also true
 select p.FirstName, p.LastName, a.City, a.State
 from Person p left join Address a on p.PersonId = a.PersonId;
