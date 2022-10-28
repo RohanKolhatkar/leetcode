19d0882b4b89343edbca90f8aae084077a7343ac
Rohan kolhatkar <rohankolhatkar930@gmail.com>
2022-10-28 20:13:00 +0530

    Time: 288 ms (61.83%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -4,8 +4,18 @@
 # using if clause and case statements.
 
 # using if
-update salary
-set sex = if(sex='m','f','m');
+# update salary
+# set sex = if(sex='m','f','m');
+
+
+# using case 
+UPDATE salary
+SET
+    sex =
+    CASE sex
+        WHEN 'm' THEN 'f'
+        ELSE 'm'
+    END;
 
 
 
