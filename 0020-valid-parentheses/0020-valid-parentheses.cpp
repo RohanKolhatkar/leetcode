ab04d35e8e113a6c17a6a7c5c0a162806811363a
Rohan kolhatkar <rohankolhatkar930@gmail.com>
2022-11-02 22:46:27 +0530

    Time: 3 ms (53.03%), Space: 6.2 MB (95.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,19 +3,16 @@ public:
     bool isValid(string s) {
         stack<char> st;
         int n = s.length();
-        int c = 0;
         
-        // if(s[0]==')' || s[0]=='}' || s[0]==']'){
-        //         return false;
-        //     }
-        // else{
+        
+        
             
            for(int i = 0; i < n; i++){   
             
                 if(st.empty() || s[i] == '(' || s[i] == '{' || s[i] == '[')
                 {
                     st.push(s[i]);
-                    c++;
+                    
                 }
                 else
                 {
@@ -27,13 +24,13 @@ public:
                         st.pop();
                     else
                         return false;
-                    c--;
+                    
                 }
-          // } 
+           
         }
         
-        if(c == 0)
-            return true;
-        return false;
+        // if(c == 0)
+        //     return true;
+        return st.empty();
     }
 };
