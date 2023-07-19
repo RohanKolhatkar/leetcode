8d7a489cddd9e5cca73bf5dba4dde8cafe6b4b93
Rohan kolhatkar <rohankolhatkar930@gmail.com>
2023-07-19 18:43:57 +0530

    Added solution - LeetHub

---
+++ Search Pattern (KMP-Algorithm) - GFG/search-pattern-kmpalgorithm.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        vector <int> search(string pat, string s)
+        {
+            vector<int>ans;
+            size_t f = s.find(pat);
+            while(f != string::npos){
+                ans.push_back(f+1);
+                f = s.find(pat,f+1);
+            }
+            return ans;
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        if (res.size()==0)
+            cout<<-1<<endl;
+        else {
+            for (int i : res) cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
